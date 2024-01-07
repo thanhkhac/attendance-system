84a1dc23cbe8e190612fe74c144b91b8ff18ee81
Thanh Nguyen Khac <thanhcqb2048@gmail.com>
2024-01-07 22:33:04 +0700

    Database demo lan 1

--- Database/AttendanceSystem.sql
+++ Database/AttendanceSystem.sql
@@ -22,7 +22,7 @@ CREATE TABLE Employees(
 	[DepartmentID] int,
 	[RoleID] int DEFAULT 1,
 	[StartDate] date,
-	[EndDate] date,
+	[EndDate] date ,
 	[IsActive] bit DEFAULT 1
 );
 
@@ -34,43 +34,95 @@ CREATE TABLE Roles(
 
 CREATE TABLE Departments(
 	[DepartmentID] int IDENTITY(1,1) PRIMARY KEY,
-	[ManagerID] int,
 	[Name] nvarchar(50),
+	[ManagerID] int,
 );
 
-CREATE TABLE Shifts(
-	[ShiftID] int IDENTITY(1,1) PRIMARY KEY,
-	[Name] nvarchar(50),
-	[StartTime] time,
-	[EndTime] time
-)
+--CREATE TABLE Shifts(
+--	[ShiftID] int IDENTITY(1,1) PRIMARY KEY,
+--	[Name] nvarchar(50),
+--	[StartTime] time,
+--	[EndTime] time
+--)
 
 CREATE TABLE Timesheet(
+	[TimeSheetID] int IDENTITY(1,1) PRIMARY KEY,
 	[Date] date,
 	[EmployeeID] int,
-	[ShiftID] int,
+--	[ShiftID] int,
 	[TimeIn] time,
 	[TimeOut] time,
-	[IsLeave] bit DEFAULT 0,
-	PRIMARY KEY ([Date], [EmployeeID])
+--	[IsLeave] bit DEFAULT 0,
+	[Note] nvarchar(max),
+	UNIQUE([Date], [EmployeeID])
 );
 
+CREATE TABLE Leaves(
+	TimeSheetID int UNIQUE,
+	Reason nvarchar(max),
+	[Status] bit,
+	[ResponedBy] int
+)
+
+
+CREATE TABLE OverTime(
+	[Date] date,
+	[EmployeeID] int,
+	[StartTime] time,
+	[EndTime] time,
+	[TimeIn] time,
+	[TimeOut] time,
+	PRIMARY KEY([Date], [EmployeeID])
+);
+
+
+CREATE TABLE News(
+	NewsID int PRIMARY KEY,
+	Title nvarchar(50),
+	[Content] nvarchar(max),
+	[DateTime] datetime,
+	CreatedBy int,
+)
 
 GO
 --=====================FOREIGN KEY
 --Employees
 ALTER TABLE Employees
 ADD CONSTRAINT FK_Employees_Departments FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+
 ALTER TABLE Employees
 ADD	CONSTRAINT FK_Employees_Roles FOREIGN KEY ([RoleID]) REFERENCES Roles([RoleID])
+
+
 --Departments
 ALTER TABLE Departments
 ADD CONSTRAINT FK_Departments_Employees FOREIGN KEY([ManagerID]) REFERENCES Employees(EmployeeID)
+
+
 --TimeSheets
 ALTER TABLE Timesheet
 ADD CONSTRAINT FK_Timesheet_Employees FOREIGN KEY ([EmployeeID]) REFERENCES Employees(EmployeeID)
-ALTER TABLE Timesheet
-ADD CONSTRAINT FK_Timesheet_Shifts FOREIGN KEY ([ShiftID]) REFERENCES Shifts(ShiftID)
+
+--ALTER TABLE Timesheet
+--ADD CONSTRAINT FK_Timesheet_Shifts FOREIGN KEY ([ShiftID]) REFERENCES Shifts(ShiftID)
+
+
+--Leaves
+ALTER TABLE Leaves
+ADD CONSTRAINT FK_Timesheet_Leaves FOREIGN KEY (TimeSheetID) REFERENCES Timesheet(TimesheetID) 
+
+ALTER TABLE Leaves
+ADD CONSTRAINT FK_Timesheet_Employees_ResponedBy FOREIGN KEY ([ResponedBy]) REFERENCES Employees(EmployeeID) 
+
+
+--OverTimes
+ALTER TABLE OverTime
+ADD CONSTRAINT FK_OverTime_Employees FOREIGN KEY ([EmployeeID]) REFERENCES Employees(EmployeeID)
+
+
+--News
+ALTER TABLE News
+ADD CONSTRAINT FK_News_Employees_CreatedBy FOREIGN KEY (CreatedBy) REFERENCES Employees(EmployeeID)
 
 GO
 --====================INSERT
@@ -95,10 +147,14 @@ DECLARE @PhongTiepThi int = (SELECT DepartmentID FROM Departments WHERE [Name] =
 INSERT INTO Employees (FirstName, MiddleName, LastName, Email, CCCD, PhoneNumber, DepartmentID, RoleID, IsActive) VALUES
 (N'Thành', N'Khắc', N'Nguyễn', 'thanhcqb2048@gmail.com', '456789012', '0382293846', @PhongTiepThi, @NhanVien, 1)
 
-INSERT INTO Shifts([Name],StartTime, EndTime) VALUES (N'Cả ngày', '7:30:00', '17:30:00')
-INSERT INTO Shifts([Name],StartTime, EndTime) VALUES (N'Ca sáng', '7:30:00', '11:30:00')
-INSERT INTO Shifts([Name],StartTime, EndTime) VALUES (N'Ca chiều', '13:30:00', '17:30:00')
+--INSERT INTO Shifts([Name],StartTime, EndTime) VALUES (N'Cả ngày', '7:30:00', '17:30:00')
+--INSERT INTO Shifts([Name],StartTime, EndTime) VALUES (N'Ca sáng', '7:30:00', '11:30:00')
+--INSERT INTO Shifts([Name],StartTime, EndTime) VALUES (N'Ca chiều', '13:30:00', '17:30:00')
 
+
+--DECLARE @CaNgay int = (SELECT ShiftID FROM Shifts WHERE [Name] = N'Cả ngày')
+--DECLARE @CaSang int = (SELECT ShiftID FROM Shifts WHERE [Name] = N'Ca sáng')
+--DECLARE @CaChieu int = (SELECT ShiftID FROM Shifts WHERE [Name] = N'Ca chiều')
 
 
 
