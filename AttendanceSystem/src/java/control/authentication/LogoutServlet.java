3336ea99018bec89068c5c05bafd3db0c2516b7b
phamtruong230903@gmail.com <phamtruong230903@gmail.com>
2024-01-29 14:01:30 +0700

    sua-login

--- AttendanceSystem/src/java/control/authentication/LogoutServlet.java
+++ AttendanceSystem/src/java/control/authentication/LogoutServlet.java
@@ -33,6 +33,7 @@ public class LogoutServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
         session.invalidate();
+        
         response.sendRedirect("cookieLogin");
     } 
 
