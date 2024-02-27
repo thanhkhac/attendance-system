4c42515f5ddde2ca1bb30e1683cf785e2af05747
Duong Nguyen <duongnmhe172724@fpt.edu.vn>
2024-02-27 22:53:27 +0700

    resignation rq

--- AttendanceSystem/src/java/model/request/ResignationRequestDAO.java
+++ AttendanceSystem/src/java/model/request/ResignationRequestDAO.java
@@ -5,7 +5,9 @@
 package model.request;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import model.EmployeeDTO;
+import ultility.datetimeutil.DateTimeUtil;
 
 /**
  *
@@ -13,6 +15,8 @@ import model.EmployeeDTO;
  */
 public class ResignationRequestDAO extends dbhelper.DAOBase {
 
+    static final DateTimeUtil DATE_UTIL = new DateTimeUtil();
+    
     public boolean insertResignationRequest(EmployeeDTO e, LocalDate sentDate, LocalDate extendDate, String reason) {
         if (connection != null) {
             try {
@@ -37,5 +41,167 @@ public class ResignationRequestDAO extends dbhelper.DAOBase {
             }
         }
         return false;
+    }
+    
+    public ArrayList<ResignationRequestDTO> getRegisnationRequestByDepartment(int departID) {
+        LeaveRequestDAO dao = new LeaveRequestDAO();
+        ArrayList<ResignationRequestDTO> list = new ArrayList<>();
+//        if (connection != null) {
+            try {
+                String sql = "SELECT ResignationRequestID,\n"
+                        + "	rr.EmployeeID,\n"
+                        + "	StartDateContract,\n"
+                        + "	EndDateContract,\n"
+                        + "	SentDate,\n"
+                        + "	ExtendDate,\n"
+                        + "	Reason,\n"
+                        + "	ManagerApprove,\n"
+                        + "	HrApprove,\n"
+                        + "	ManagerID,\n"
+                        + "	HrID,\n"
+                        + "	[Status],\n"
+                        + "	e.DepartmentID\n"
+                        + "FROM ResignationRequests rr\n"
+                        + "JOIN Employees e ON rr.EmployeeID = e.EmployeeID\n"
+                        + "WHERE e.DepartmentID like '%" + departID + "%'\n"
+                        + "ORDER BY ManagerApprove ASC , ResignationRequestID ASC";
+                ps = connection.prepareStatement(sql);
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    int resignation = rs.getInt("ResignationRequestID");
+                    int employeeID = rs.getInt("EmployeeID");
+                    int departmentID = rs.getInt("DepartmentID");
+                    LocalDate startDateContract = DATE_UTIL.parseSqlDate(rs.getDate("StartDateContract"));
+                    LocalDate endDateContract = DATE_UTIL.parseSqlDate(rs.getDate("EndDateContract"));
+                    LocalDate sentDate = DATE_UTIL.parseSqlDate(rs.getDate("SentDate"));
+                    LocalDate extendDate = DATE_UTIL.parseSqlDate(rs.getDate("ExtendDate"));
+                    String reason = rs.getString("Reason");
+                    Boolean managerApprove = dao.parseBoolean(rs.getString("ManagerApprove"));
+                    Boolean hrApprove = dao.parseBoolean(rs.getString("HrApprove"));
+                    int managerID = rs.getInt("ManagerID");
+                    int hrID = rs.getInt("HrID");
+                    Boolean status = dao.parseBoolean(rs.getString("Status"));
+
+                    ResignationRequestDTO resign = new ResignationRequestDTO(resignation, employeeID, departmentID, startDateContract, endDateContract, sentDate, extendDate, reason, managerApprove, hrApprove, managerID, hrID, status);
+                    list.add(resign);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println(e);
+            } finally {
+                closeAll();
+            }
+//        }
+        return list;
+    }
+
+    public ArrayList<ResignationRequestDTO> getRegisnationRequestForHR(int approve) {
+        LeaveRequestDAO dao = new LeaveRequestDAO();
+        ArrayList<ResignationRequestDTO> list = new ArrayList<>();
+//        if (connection != null) {
+            try {
+                String sql = "SELECT ResignationRequestID,\n"
+                        + "	rr.EmployeeID,\n"
+                        + "	StartDateContract,\n"
+                        + "	EndDateContract,\n"
+                        + "	SentDate,\n"
+                        + "	ExtendDate,\n"
+                        + "	Reason,\n"
+                        + "	ManagerApprove,\n"
+                        + "	HrApprove,\n"
+                        + "	ManagerID,\n"
+                        + "	HrID,\n"
+                        + "	[Status],\n"
+                        + "	e.DepartmentID\n"
+                        + "FROM ResignationRequests rr\n"
+                        + "JOIN Employees e ON rr.EmployeeID = e.EmployeeID\n"
+                        + "WHERE rr.ManagerApprove like '%" + approve + "%'\n"
+                        + "ORDER BY HrApprove ASC , ResignationRequestID ASC";
+                ps = connection.prepareStatement(sql);
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    int resignation = rs.getInt("ResignationRequestID");
+                    int employeeID = rs.getInt("EmployeeID");
+                    int departmentID = rs.getInt("DepartmentID");
+                    LocalDate startDateContract = DATE_UTIL.parseSqlDate(rs.getDate("StartDateContract"));
+                    LocalDate endDateContract = DATE_UTIL.parseSqlDate(rs.getDate("EndDateContract"));
+                    LocalDate sentDate = DATE_UTIL.parseSqlDate(rs.getDate("SentDate"));
+                    LocalDate extendDate = DATE_UTIL.parseSqlDate(rs.getDate("ExtendDate"));
+                    String reason = rs.getString("Reason");
+                    Boolean managerApprove = dao.parseBoolean(rs.getString("ManagerApprove"));
+                    Boolean hrApprove = dao.parseBoolean(rs.getString("HrApprove"));
+                    int managerID = rs.getInt("ManagerID");
+                    int hrID = rs.getInt("HrID");
+                    Boolean status = dao.parseBoolean(rs.getString("Status"));
+
+                    ResignationRequestDTO resign = new ResignationRequestDTO(resignation, employeeID, departmentID, startDateContract, endDateContract, sentDate, extendDate, reason, managerApprove, hrApprove, managerID, hrID, status);
+                    list.add(resign);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println(e);
+            } finally {
+                closeAll();
+            }
+//        }
+        return list;
+    }
+    
+    public boolean approvalOfResignationByManager(int status, int managerID, int requestID) {
+//        if (connection != null) {
+            try {
+                String sql = "UPDATE ResignationRequests \n"
+                        + "SET ManagerApprove = ? , ManagerID = ? \n"
+                        + "WHERE ResignationRequestID = ?";
+                ps = connection.prepareStatement(sql);
+                ps.setInt(1, status);
+                ps.setInt(2, managerID);
+                ps.setInt(3, requestID);
+                int row = ps.executeUpdate();
+                if (row > 0) {
+                    return true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println(e);
+            } finally {
+                closeAll();
+            }
+//        }
+
+        return false;
+    }
+
+    public boolean approvalOfResignationByHR(int status, int managerID, int requestID) {
+//        if (connection != null) {
+            try {
+                String sql = "Update ResignationRequests \n"
+                        + "set HrApprove = ? , HrID = ? \n"
+                        + "where ResignationRequestID = ?";
+                ps = connection.prepareStatement(sql);
+                ps.setInt(1, status);
+                ps.setInt(2, managerID);
+                ps.setInt(3, requestID);
+                int row = ps.executeUpdate();
+                if (row > 0) {
+                    return true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println(e);
+            } finally {
+                closeAll();
+            }
+//        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        ResignationRequestDAO rrDao = new ResignationRequestDAO();
+//        ArrayList<ResignationRequestDTO> list1 = rrDao.getRegisnationRequestForHR(1);
+        ArrayList<ResignationRequestDTO> list2 = rrDao.getRegisnationRequestByDepartment(2);
+//        System.out.println("List 1 size : " + list1.size());
+        System.out.println("List 1 size : " + list2.size());
+
     }
 }
